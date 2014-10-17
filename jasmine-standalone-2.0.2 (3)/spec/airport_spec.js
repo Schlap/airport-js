94c74753800ff925fb0fdaa04b22f8731b37d4b6
Hadi <hadichalabi@gmail.com>
2014-10-17 11:59:39 +0100

    airport can land plane, plane can takeoff

---
+++ jasmine-standalone-2.0.2 (3)/spec/airport_spec.js
@@ -0,0 +1,40 @@
+describe('Airport', function () {
+
+  beforeEach(function(){
+
+    airport = new Airport
+    plane = new Plane
+
+  });
+
+  it('is empty', function () {
+
+    expect(airport.planes).toEqual([])
+
+  });
+
+  it('can hold a planes', function () {
+     jumbo = new Plane
+     boeing = new Plane
+     airport.land(jumbo)
+     airport.land(boeing)
+     expect(airport.planes).toEqual([jumbo, boeing])
+
+  });
+
+  xit('can not land the same plane twice', function(){
+    airport.land(plane)
+    airport.land(plane)
+    expect(airport.planes).toEqual('This plane has already landed')
+
+  });
+
+  it('can allow plane to takeoff', function () {
+    airport.land(plane)
+    airport.takeOff(plane)
+    expect(airport.planes).toEqual([])
+
+
+  });
+
+});
